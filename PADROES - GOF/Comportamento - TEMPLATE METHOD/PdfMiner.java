c336043beda29c3a94ef0ecef73f93d5d668b565
Igor Mariano <igor-mariano@outlook.com>
2023-09-09 10:12:30 -0300

    Criando o arquivo PdfMiner.java

---
+++ PADROES - GOF/Comportamento - TEMPLATE METHOD/PdfMiner.java
@@ -0,0 +1,30 @@
+// Classe concreta para mineração de documentos PDF
+
+import java.io.File;
+
+class PdfMiner extends DocumentMiner {
+    private File pdfFile;
+
+    @Override
+    protected void openDocument(String filePath) {
+        System.out.println("Abrindo documento PDF: " + filePath);
+        pdfFile = new File(filePath);
+    }
+
+    @Override
+    protected void mineData() {
+        System.out.println("Extraindo dados de um documento PDF");
+        System.out.println("Status da leitura: Lendo arquvivo...");
+    }
+
+    @Override
+    protected void closeDocument() {
+        System.out.println("Leitura concluída com sucesso !");
+        System.out.println("Fechando documento PDF: " + pdfFile.getName());
+    }
+
+    @Override
+    protected void nextDocument() {
+        System.out.println("\n---------- Próximo Arquivo ----------");
+    }
+}
