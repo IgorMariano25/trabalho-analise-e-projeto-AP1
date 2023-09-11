af8bce67d34f8a61bdaf0241300e8a2cd16fda61
Igor Mariano <igor-mariano@outlook.com>
2023-09-11 09:50:15 -0300

    Criando o arquivo MainSRP.java

---
+++ SOLID/SRP/MainSRP.java
@@ -0,0 +1,18 @@
+package SOLID.SRP;
+
+public class MainSRP {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("João", 5000.0);
+        
+        System.out.println("  Antes de aplicar o SRP  ");
+        System.out.println("----------------------------");
+        System.out.println("Relatório do Funcionário:");
+        System.out.println(funcionario.gerarRelatorio());
+
+        System.out.println("\n  Depois de aplicar o SRP  ");
+        System.out.println("----------------------------");
+        GeradorDeRelatorio gerador = new GeradorDeRelatorio();
+        System.out.println("Relatório do Funcionário:");
+        System.out.println(gerador.gerarRelatorio(funcionario));
+    }
+}
