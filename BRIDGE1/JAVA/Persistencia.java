af76dad24f804c31b753d5f9d91041b4cb832d8f
Igor Mariano <igor-mariano@outlook.com>
2023-09-11 08:53:21 -0300

    Ignorando arquivos no .gitignore

--- BRIDGE1/JAVA/Persistencia.java
+++
@@ -1,5 +0,0 @@
-package JAVA;
-public interface Persistencia {
-    void salvar(Publicacao publicacao);
-    void recuperar(Publicacao publicacao);
-}
--- PROXY/Imagem.java
+++
@@ -1,3 +0,0 @@
-public interface Imagem {
-    public void display();
-}
