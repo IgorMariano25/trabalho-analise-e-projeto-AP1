1df542184f86619e11f9649e0e7d313c2c472e1b
Igor Mariano <igor-mariano@outlook.com>
2023-09-11 08:28:46 -0300

    Criando o arquivo ProxyRecurso.java

---
+++ PADROES - GOF/Estrutura - PROXY/ProxyRecurso.java
@@ -0,0 +1,34 @@
+// package PADROES - GOF.Estrutura - PROXY;
+class ProxyRecurso implements IRecurso {
+    private RecursoReal recursoReal;
+    private String nomeRecurso;
+    private String nomeUsuario;
+    private String senha;
+
+    public ProxyRecurso(String nomeRecurso, String nomeUsuario, String senha) {
+        this.nomeRecurso = nomeRecurso;
+        this.nomeUsuario = nomeUsuario;
+        this.senha = senha;
+    }
+
+    public void acessar() {
+        if (autenticar()) {
+            if (recursoReal == null) {
+                recursoReal = new RecursoReal(nomeRecurso);
+            }
+            System.out.println("------------------------------------------------");
+            System.out.println(" [ OK ] Acesso Permitido: Credenciais Corretas  ");
+            System.out.println("------------------------------------------------");
+            recursoReal.acessar();
+        } else {
+            System.out.println("------------------------------------------------");
+            System.out.println(" [ X ] Acesso negado: Credenciais Inválidas     ");
+            System.out.println("------------------------------------------------");
+            System.out.println("Você não tem permissão para acessar o recurso: " + this.nomeRecurso);
+        }
+    }
+
+    private boolean autenticar() {
+        return "usuario".equals(nomeUsuario) && "senha123".equals(senha);
+    }
+}
