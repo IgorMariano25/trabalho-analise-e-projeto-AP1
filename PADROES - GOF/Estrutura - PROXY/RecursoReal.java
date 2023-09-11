6dc09a7bc39d56f4a6a62791665d669bf512257e
Igor Mariano <igor-mariano@outlook.com>
2023-09-11 08:28:31 -0300

    Criando o arquivo RecursoReal.java

---
+++ PADROES - GOF/Estrutura - PROXY/RecursoReal.java
@@ -0,0 +1,11 @@
+class RecursoReal implements IRecurso {
+    private String nomeRecurso;
+
+    public RecursoReal(String nomeRecurso) {
+        this.nomeRecurso = nomeRecurso;
+    }
+
+    public void acessar() {
+        System.out.println("Acessando o recurso real: " + nomeRecurso);
+    }
+}
