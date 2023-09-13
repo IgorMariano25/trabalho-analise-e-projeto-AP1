122bbacde3f30f1a398257b36150aa28b189625f
Igor Mariano <igor-mariano@outlook.com>
2023-09-13 16:42:40 -0300

    Criando o arquivo DesenhoAPI2.java

---
+++ PSOO/BRIDGE/DesenhoAPI2.java
@@ -0,0 +1,6 @@
+class DesenhoAPI2 implements DesenhoAPI {
+    @Override
+    public void desenharCirculo(int x, int y, int raio) {
+        System.out.printf("API2 desenha o círculo no ponto (%d, %d) com raio %d%n", x, y, raio);
+    }
+}
